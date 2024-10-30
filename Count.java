e7a5456bb2a402e01bf517f4bbf75db94ef7836a
anurag-dev89 <anuragpatel212121gmail.com>
2024-10-30 13:18:25 +0530

    first problem commited

---
+++ Count.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Count {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+        while(true) {
+            System.out.print("Enter a number (or 'q' to quit): ");
+            String input = scanner.next();
+
+            if(input.equals("q")){
+                break;
+            } else {
+                int number = Integer.parseInt(input);
+                
+                if (number == 0) {
+                    zeroCount++;
+                } else if (number > 0) {
+                    positiveCount++;
+                } else {
+                    negativeCount++;
+                }
+            }
+        }
+
+        System.out.println("Count of positive numbers: " + positiveCount);
+        System.out.println("Count of negative numbers: " + negativeCount);
+        System.out.println("Count of zeros: " + zeroCount);
+    }
+}
